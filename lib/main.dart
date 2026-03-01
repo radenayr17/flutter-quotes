de175444287c6fc61dcc1fdc145e459dd59ce16a
Cesar Ryan Edar <radenayr@192.168.1.6>
2026-03-01 21:05:33 +0800

    Custom Class and Cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:quotes_list/qoute.dart';
+
 void main() {
   runApp(MaterialApp(home: QuoteList()));
 }
@@ -12,33 +14,67 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    'This is my first quote',
-    'This is my second quote',
-    'This is my third quote',
+  List<Qoute> quotes = [
+    Qoute(
+      text:
+          'The only limit to our realization of tomorrow will be our doubts of today.',
+      author: 'Franklin D. Roosevelt',
+    ),
+    Qoute(
+      text:
+          'Life is not measured by the number of breaths we take, but by the moments that take our breath away.',
+      author: 'Maya Angelou',
+    ),
+    Qoute(
+      text:
+          'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.',
+      author: 'Dr. Seuss',
+    ),
   ];
+
+  Widget quoteTemplate(Qoute quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              '"${quote.text}"',
+              textAlign: TextAlign.justify,
+              style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+            ),
+            SizedBox(height: 6),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[800],
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('Quotes'),
+        title: Text(
+          'Quotes',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) {
-          return Center(
-            child: Card(
-              margin: EdgeInsets.all(5),
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Text(quote, style: TextStyle(fontSize: 18)),
-              ),
-            ),
-          );
-        }).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
---
+++ lib/qoute.dart
@@ -0,0 +1,6 @@
+class Qoute {
+  String text;
+  String author;
+
+  Qoute({required this.text, required this.author});
+}
